cf89198f7584fd25e49471317649e94fa423f7f9
gopal.lin <gopal.lin@evolutivelabs.com>
2024-03-31 16:43:13 +0800

    feat: add request log function

---
+++ database/migrations/2023_12_25_105727_create_request_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_ip', 100);
+            $table->string('http_method', 100);
+            $table->string('domain', 100);
+            $table->string('path', 100);
+            $table->longText('payload')->nullable();
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request_logs');
+    }
+};
